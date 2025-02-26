c358ca6633e1ba41707f5bce07add61228e552e6
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-02-26 14:40:19 +0100

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+
+puts "Cleaned database"
+
+Restaurant.create(name: "Chez Gusto", address: "2 rue du boulanger", category: "french")
+Restaurant.create(name: "Sushi Palace", address: "4 rue des cerisiers", category: "japonese")
+Restaurant.create(name: "Délices soufflés", address: "15 boulevard Brioche", category: "french")
+Restaurant.create(name: "Al Dente", address: "46 rue des tomates" , category: "italian")
+Restaurant.create(name: "Roulé Boulé", address: "53 rue du Tigre Blanc", category: "chinese" )
+
+puts "My five restaurants created"
